6de5db9347eedca444ca6614e1a7ecde2d6e7af1
DigitalNotebook <16731993+DigitalNotebook@users.noreply.github.com>
2020-12-05 23:42:42 +1000

    .

---
+++ General Windows/Get-WMIComputer.ps1
@@ -0,0 +1,80 @@
+﻿Function  Get-WMIComputer
+{
+  [cmdletBinding()]
+  param(
+
+    [Parameter(Mandatory                =$True,
+        ValueFromPipeline               =$true,
+        ValueFromPipelineByPropertyName =$True)]
+    [Alias('Hostname', 'cn', 'Name')]  # Flexible ByPropertyName or use Hastable @{ComputerName = "Name";Expression={$_.ComputerName}}
+    [String[]]$Computername
+  )
+  
+  
+  Begin {}
+  Process # Process block is needed to get more than one object out of the pipeline           
+  { # Process block is ignored when there is no pipleline input
+     
+    Foreach ($Computer in $Computername) { 
+
+      try {
+
+        $CimSessionOption = New-CimSessionOption -Protocol Dcom
+        $CimSession = New-CimSession -ComputerName $Computer -SessionOption $CimSessionOption -ErrorAction Stop             
+   
+        $ComputerSystem  = Get-CimInstance -Class Win32_ComputerSystem  -CimSession $CimSession    
+        $OperatingSystem = Get-CimInstance -Class Win32_OperatingSystem -CimSession $CimSession   
+    
+          #Splatting hash table
+          $Properties         =  @{
+          Status              = 'Connected CimSession'
+          TotalPhysicalMemory = $ComputerSystem.TotalPhysicalMemory
+          Manufacturer        = $ComputerSystem.Manufacturer
+          model               = $ComputerSystem.Model
+          SystemType          = $ComputerSystem.SystemType
+               
+          RegisteredUser      = $OperatingSystem.RegisteredUser
+          OSArchitecture      = $OperatingSystem.OSArchitecture}   
+
+      } catch {
+
+          $Properties         = @{
+          Status              = 'Failed CimSession'
+          TotalPhysicalMemory = $null
+          Manufacturer        = $null
+          model               = $null
+          SystemType          = $null
+               
+          RegisteredUser      = $null
+          OSArchitecture      = $null} 
+       
+       
+        Write-Verbose $error[0]                              
+      
+      } finally {
+      
+        $obj = New-Object -TypeName psobject -Property $Properties              
+      
+      
+  
+        if ($obj.status -eq 'Connected CimSession')
+        {
+          Remove-CimSession -CimSession $CimSession
+       
+        }
+        Write-Output  $obj
+      
+      }
+ 
+    }
+  }
+  
+ 
+
+  
+  End {}
+
+}  
+
+  "localhost", "127.0.0.1" | Get-WMIComputer  
+ 

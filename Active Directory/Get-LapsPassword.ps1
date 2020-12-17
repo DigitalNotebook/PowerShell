0f4b604902f219c7ed260de9ba7e217d3269473c
DigitalNotebook <16731993+DigitalNotebook@users.noreply.github.com>
2020-12-18 00:18:04 +1000

    .

---
+++ Active Directory/Get-LapsPassword.ps1
@@ -0,0 +1,26 @@
+﻿ 
+ 
+Function Get-LapsPassword
+{
+  [cmdletBinding()]
+  param(
+
+    [Parameter(Mandatory=$True,
+        ValueFromPipeline=$true,
+        ValueFromPipelineByPropertyName =$True)]
+    [Alias('hostname', 'cn', 'Name', 'Computer', 'PC')]  # Flexible ByPropertyName or use Hastable @{Name = 'Name'; Expression={$_.ComputerName}}
+    [String[]]$Computername
+  )
+
+  foreach ($Computer in $Computername)
+  {
+
+  Get-ADComputer -Identity $Computer -Properties ms-Mcs-AdmPwd, ms-Mcs-AdmPwdExpirationTime `
+  | Select-Object `
+    @{N='ComputerName'; E={$_.name}}, `
+    @{N='ComputerPassword'; E={$_."ms-Mcs-AdmPwd"}}, `
+    @{N='PasswordExpirationTime'; E={[DateTime]::FromFileTime($_."ms-Mcs-AdmPwdExpirationTime")}}
+
+  
+  }
+}

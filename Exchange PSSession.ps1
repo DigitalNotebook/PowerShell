c9e2d124f9ceae36db8ddbcf04a4f948ae608c85
DigitalNotebook <16731993+DigitalNotebook@users.noreply.github.com>
2020-12-20 09:27:39 +1000

    .

--- Exchange PSSession.ps1
+++
@@ -1,4 +0,0 @@
-$UserCredential = Get-Credential
-$ESEX16 = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://server/powershell -Authentication Kerberos -Credential $UserCredential
-Import-PSSession $ESEX16 -allowclobber  
-#remove-pssession $ESEX16
